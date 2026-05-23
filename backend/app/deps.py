c4aa467d7f5fd4f08556e6cf761c6e00604c465e
Mandela Offonry <78932067+Offonry1@users.noreply.github.com>
2026-05-23 14:14:10 +0100

    Change get_db to a synchronous function

--- backend/app/deps.py
+++ backend/app/deps.py
@@ -6,7 +6,7 @@ from sqlmodel import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
-async def get_db():
+def get_db():
     yield from get_session()
 
 async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
